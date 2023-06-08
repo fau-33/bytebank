685b66659a6ebce9b7caad79cbf557b7a49cd1e6
Flávio Félix <fau_felix30@hotmail.com>
2023-06-08 20:33:30 -0300

    Refazendo os métodos

--- src/Conta.java
+++ src/Conta.java
@@ -16,4 +16,14 @@ public class Conta {
 			return false;
 		}
 	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= saldo) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}else {
+			return false;
+		}
+	}
 }
--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -5,9 +5,22 @@ public class TestaMetodo {
 		contaDoFlavio.saldo = 100;
 		contaDoFlavio.deposita(50);
 		System.out.println(contaDoFlavio.saldo);
-		
+
 		boolean conseguiuRetirar = contaDoFlavio.saca(20);
 		System.out.println(contaDoFlavio.saldo);
 		System.out.println(conseguiuRetirar);
+
+		Conta contaDeMarcela = new Conta();
+		contaDeMarcela.deposita(1000);
+
+		boolean sucessoTranferencia = contaDeMarcela.transfere(300, contaDoFlavio);
+
+		if (sucessoTranferencia) {
+			System.out.println("Transferência feita com sucesso!");
+		} else {
+			System.out.println("Faltou dinheiro");
+		}
+		System.out.println(contaDeMarcela.saldo);
+		System.out.println(contaDoFlavio.saldo);
 	}
 }
